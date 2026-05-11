8a0650ffd5e38c92478d5df8b1ea440a35bcc2a4
abdullahazam-708 <abdullahtech708@gmail.com>
2026-05-11 23:29:00 +0500

    Upload frontend and backend

---
+++ Backend/Backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const accountRoutes = require("./routes/accountRoutes")
+const notesRoutes = require("./routes/notesRoutes")
+const connectDB = require("./config/db")
+const cors  = require("cors")
+
+connectDB()
+
+const app = express();  
+app.use(express.json())
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+app.use("/account",accountRoutes)
+app.use("/notes",notesRoutes)
+
+
+const port = 3001;
+
+app.listen(port,() => {
+    console.log(`Server is running at port: ${port}`)
+})
